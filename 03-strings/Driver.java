aafccdb51565bc0ec274be67a946a5fa3a382e02
TiffL <TiffL@users.noreply.github.com>
2014-09-21 14:12:35 -0400

    Update Driver.java

--- 03-strings/Driver.java
+++ 03-strings/Driver.java
@@ -1,9 +1,9 @@
 public class Driver {
     public static void main(String[] args){
         String s = "harry potter";
-        int space = s.indexOf(" ");
-	String first = s.substring(0,space);
-	String last = s.substring(space + 1);
+        int spaceloc = s.indexOf(" ");
+	String first = s.substring(0,spaceloc);
+	String last = s.substring(spaceloc + 1);
 
 	//System.out.println(first);
 	//System.out.println(last);
