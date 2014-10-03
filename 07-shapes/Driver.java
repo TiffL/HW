ec24387cb4db374bd8324f6ed3e3c67066fdce65
TiffL <lei.tiffany@gmail.com>
2014-10-02 21:35:14 -0400

    HW Assignments 7 and 7b

--- 07-shapes/Driver.java
+++ 07-shapes/Driver.java
@@ -13,5 +13,20 @@ public class Driver{
 	System.out.println("~~~Tri2 Tests:~~~\n");
 	System.out.println(s.tri2(4));
 	System.out.println(s.tri2(3));
+
+	System.out.println("~~~Tri3 Tests:~~~\n");
+	System.out.println(s.tri3(4));
+	System.out.println(s.tri3(3));
+
+	System.out.println("~~~Diamond Tests:~~~\n");
+	System.out.println(s.diamond(7));
+	System.out.println(s.diamond(5));
+	System.out.println(s.diamond(3));
+	System.out.println(s.diamond(4));
+	System.out.println(s.diamond(6));
+
+	System.out.println("~~~Tri4 Tests:~~~\n");
+	System.out.println(s.tri4(4));
+	System.out.println(s.tri4(3));
     }
 }
--- 07-shapes/Shapes.java
+++ 07-shapes/Shapes.java
@@ -54,4 +54,79 @@ public class Shapes {
 	}
 	return retstr;
     }
+    
+    //tri3: 20 minutes
+    public String tri3(int h){
+	int nOfStars = 1;
+	int maxStars = h*2-1;
+	String retstr = "";
+
+	for (int r = 0; r < h; r++){
+	    for (int s = 0; s < (maxStars - nOfStars)/2; s++){
+		retstr = retstr + " ";
+	    }
+	    for (int stars = 0; stars < nOfStars; stars++){
+		retstr = retstr + "*";
+	    }
+	    retstr = retstr + "\n";
+	    nOfStars = nOfStars + 2;
+	}
+	return retstr;
+    }
+
+    //diamond: 25 minutes
+    public String diamond(int h){
+    	int nOfStars = 0;
+    	String retstr = "";
+
+	int middlerow = 0;
+	if (h%2 == 1){
+	    middlerow = h/2+1;
+	    nOfStars = h;
+	}
+	else{
+	    middlerow = h/2;
+	    nOfStars = h-1;
+	} 
+
+    	for (int r = middlerow; r > 0; r--){
+	    String row = "";
+    	    for (int s = 0; s < (h - nOfStars)/2; s++){
+    		row = row + " ";
+    	    }
+    	    for (int stars = 0; stars < nOfStars; stars++){
+    		row = row + "*";
+    	    }
+	    if (r == h/2+1){
+		retstr = row + "\n";
+	    }
+	    else{
+		retstr = row + "\n" + retstr + row + "\n";
+	    }
+    	    nOfStars = nOfStars - 2;
+    	}
+    	return retstr;
+    }
+
+    //tri4: 3 minutes -> flipped tri2
+    public String tri4(int h){
+        String retstr = "";
+
+	for (int r=h; r>0; r=r-1){
+	    int x = h-r;
+	    int c = 0;
+
+	    while (c < x){
+		retstr = retstr + " ";
+		c = c + 1;
+	    }
+	    while (c < h){
+		retstr = retstr + "*";
+		c = c + 1;
+	    }
+	    retstr = retstr + "\n";
+	}
+	return retstr;
+    }
+    
 }
