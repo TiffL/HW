255bd528b808a79dac9b1ee4b12ec7da3889d8a7
TiffL <lei.tiffany@gmail.com>
2014-11-01 00:50:41 -0400

    Continued work on maxMirror (under CodingBatC)

--- 14-arrayStuff/ArrayStuff.java
+++ 14-arrayStuff/ArrayStuff.java
@@ -162,6 +162,47 @@ public class ArrayStuff {
 	return fin;
     }
 
+    public int maxMirror(int[] nums) {
+	int largest = 0;
+
+	int[] backwards = new int[nums.length];
+	for (int i = nums.length-1; i >= 0; i--){
+	    backwards[i] = nums[nums.length-1-i];
+	}
+	System.out.println(this.toString(backwards));
+
+	int fronti = 0;
+	int backi = 0;
+	while (backi < nums.length){
+	    while (fronti < nums.length){
+		if (nums[fronti] == backwards[backi]){
+		    int indivlargest = 0;
+		    for (int i = 0; i < nums.length-fronti; i++){
+			//System.out.println(nums[fronti+i]);
+			if (nums[fronti + i] == backwards[backi + i]){
+			    indivlargest += 1;
+			}
+			else{
+			    break;
+			}
+		    }
+		    if (indivlargest > largest){
+			largest = indivlargest;
+		    }
+		    //System.out.println("indivlargest" + indivlargest);
+		}
+		// else{
+		//     backi += 1;
+		// }
+		fronti += 1;
+		//System.out.println(fronti);
+	    }
+	    backi += 1;
+	    //System.out.println(backi);
+	}
+	return largest;
+    }
+
     /*--------------------- Main --------------------*/
     public static void main(String[] args) {
 	ArrayStuff s = new ArrayStuff(10);
