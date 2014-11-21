f4cc88b03021a939afb618eb3d6931c1254a8928
TiffL <lei.tiffany@gmail.com>
2014-11-20 22:06:24 -0500

    HW Assignments 18 and 18b

--- 18-WordSearch/WordSearch.java
+++ 18-WordSearch/WordSearch.java
@@ -65,10 +65,51 @@ public class WordSearch {
         addWordH(backwards, row, col);
     }
 
+    public void addWordV(String w, int row, int col){
+	int r = row;
+	boolean possibleLoc = true;
+	if ((w.length() + row <= board.length && row >= 0)&&
+	    (col >= 0 && col < board.length)){
+	    for (int i = 0; i < w.length(); i++){
+		if ((board[i+row][col] != '.')&&
+		    (board[i+row][col] != w.charAt(i))){
+		    possibleLoc = false;
+		    break;
+		}
+	    }
+	    if (possibleLoc){
+		for (int i=0; i < w.length();i++){
+		    board[r][col] = w.charAt(i);
+		    r++;
+		}
+	    }
+	    else {
+		System.out.println("illegal overlap");
+	    }
+	}
+	else{
+	    if ((w.length()+col > board[0].length)||(col<0)){
+		System.out.println("illegal column");
+	    }
+	    else{
+		System.out.println("illegal row");
+	    }
+	}
+    }
+
+    public void addWordVBackwards(String w, int row, int col){
+	String backwards = "";
+	for (int i = w.length()-1; i >= 0; i--){
+	    backwards += w.charAt(i);
+	}
+	addWordV(backwards, row, col);
+    }
 
     public static void main(String[] args) {
 	WordSearch w = new WordSearch();
 	//System.out.println(w);
+
+	//~~~~Tests for Horizontal~~~~
 	System.out.println("1");
 	w.addWordH("hello",3,15); // 1. should work
 	System.out.println(w);
@@ -91,6 +132,26 @@ public class WordSearch {
 
 	System.out.println("6");
 	w.addWordHBackwards("hola",7,10); // 6. test backwards
+	System.out.println(w);
+
+	//~~~~Tests for Vertical~~~~
+	System.out.println("1");
+	w.addWordV("hello",3,15); // 1. should work
+	System.out.println(w);
+	System.out.println("2");
+	w.addWordV("look",4,15); // 2. test illegal overlap
+	System.out.println(w);
+	System.out.println("3");
+	w.addWordV("look",6,15); // 3. test legal overlap
+	System.out.println(w);
+	System.out.println("4");
+	w.addWordV("look",-3,20); // 4. test illegal row
+	System.out.println(w);
+	System.out.println("5");
+	w.addWordV("look",3,55); // 5. test illegal col
+	System.out.println(w);
+	System.out.println("6");
+	w.addWordVBackwards("hola",7,10); // 6. test backwards
 	System.out.println(w);
     }
 }
