3cf923fb5911303843cb0aa8d6a7bafb0842a990
TiffL <lei.tiffany@gmail.com>
2014-12-11 06:06:10 -0500

    HW Assignment 21: 21b added

--- 21-SortingClasses/Driver.java
+++ 21-SortingClasses/Driver.java
@@ -1,5 +1,36 @@
+import java.util.*;
+import java.io.*;
+
 public class Driver{
     public static void main(String[] args){
+    	ArrayList<Basechar> a1 = new ArrayList<Basechar>();
+    	 
+        Ninja e1 = new Ninja("Alien"); 
+        Ninja e2 = new Ninja("Star"); 
+        Ninja e3 = new Ninja("Fire"); 
+        
+        Cowboy e4 = new Cowboy("Teddy"); 
+        Cowboy e5= new Cowboy("Jump"); 
+        
+    	a1.add(e1);
+    	a1.add(e2);
+    	a1.add(e3);
+    	a1.add(e4);
+    	a1.add(e5);
+
+	System.out.println(a1);
+        Collections.sort(a1);        
+        System.out.println("Test Basechar arraylist: " + a1.toString());
+        
+        Ninja[] a2 = new Ninja[3];
+        a2[0] = e1;
+        a2[1] = e2;
+        a2[2] = e3;
+        
+        Arrays.sort(a2);
+        System.out.println("Test Ninja array: " + Arrays.toString(a2));
+
+	/*
 	Interval i1 = new Interval(5,50);
 	Interval i2 = new Interval();
 		
@@ -25,6 +56,7 @@ public class Driver{
 	System.out.println("Interval 6: " + i6);
 
 	System.out.println("Interval 5 compared to 6: "+i5.compareTo(i6));
-	System.out.println("Interval 6 compared to 5: "+i6.compareTo(i5));		
+	System.out.println("Interval 6 compared to 5: "+i6.compareTo(i5));
+	*/
     }
 }
